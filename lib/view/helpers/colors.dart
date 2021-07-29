ac47e17fbf1fafb31d9d57d5597fe6221f15591c
ImDeepakS <deepak.flutter.flymediatech@gmail.com>
2021-07-29 09:43:00 +0530

    Adding new changer

--- lib/view/helpers/colors.dart
+++ lib/view/helpers/colors.dart
@@ -4,3 +4,13 @@ Color mainColor = Color(0xfff7e841);
 Color black = Colors.black;
 Color shadyGrey = Colors.grey.shade400;
 Color dimGrey = Colors.grey.shade500;
+
+class SizeConfig {
+  static double? height;
+  static double? width;
+
+  void init(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+  }
+}
